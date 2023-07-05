e2c4884227dba7c3f7ca880859d551d4e2ba3057
Krzysztof Mickiewicz <107584697+Warmick91@users.noreply.github.com>
2023-07-05 23:07:18 +0200

    Started replacing interactable buttons with pushable buttons (1-3 ready)

--- Assets/CustomScripts/TouchButton.cs
+++ Assets/CustomScripts/TouchButton.cs
@@ -4,50 +4,61 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class TouchButton : XRBaseInteractable
 {
     [Header("Button Data")]
     [SerializeField] string m_buttonSign;
-    MeshRenderer m_buttonMesh;
+    [SerializeField] MeshRenderer m_visualButton;
     Color originalButtonColor;
     Color pressedButtonColor;
+    Vector3 initialButtonPosition;
+    Vector3 pressedButtonPosition;
 
     [SerializeField] Dispenser dispenser;
     TextMeshProUGUI dispenserScreenText;
     private static TouchButton m_pressedButton = null;
 
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    bool isPressed = false;
+
     void Start()
     {
-        m_buttonMesh = GetComponent<MeshRenderer>();
-        originalButtonColor = m_buttonMesh.material.color;
+        originalButtonColor = m_visualButton.material.color;
         pressedButtonColor = Color.green;
-
+        initialButtonPosition = m_visualButton.transform.localPosition;
+        pressedButtonPosition = initialButtonPosition - new Vector3(0, 0.03f, 0);
         dispenserScreenText = dispenser.DispenserScreenText;
     }
 
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
         base.OnHoverEntered(args);
-        if (m_buttonMesh != null && m_pressedButton == null)
+        if (m_visualButton != null && m_pressedButton == null && !isPressed)
         {
+            isPressed = true;
             m_pressedButton = this;
-            m_buttonMesh.material.color = pressedButtonColor;
-            AlterVisibleSequence();
+            m_visualButton.material.color = pressedButtonColor;
+            m_visualButton.transform.localPosition = pressedButtonPosition;
+            onPress?.Invoke();
         }
     }
 
     protected override void OnHoverExited(HoverExitEventArgs args)
     {
         base.OnHoverExited(args);
-        if (m_buttonMesh != null && m_pressedButton == this)
-        {
+        if (m_visualButton != null && m_pressedButton == this && isPressed)
+        {   
+            isPressed = false;
             m_pressedButton = null;
-            m_buttonMesh.material.color = originalButtonColor;
+            m_visualButton.material.color = originalButtonColor;
+            m_visualButton.transform.localPosition = initialButtonPosition;
         }
     }
 
-    private void AlterVisibleSequence()
+    public void AlterVisibleSequence()
     {
         switch (m_buttonSign)
         {
